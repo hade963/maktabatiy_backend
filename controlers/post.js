616f761af966cde4d1aebba571d280aa2753363c
hade963 <86708561+hade963@users.noreply.github.com>
2023-05-24 11:20:17 +0300

    fix query errors

--- controlers/post.js
+++ controlers/post.js
@@ -259,14 +259,14 @@ exports.get_posts = [
       p.price, 
       p.createddate, 
       p.image,
-      IF(pl.postid = p.id, true, false) AS isLiked,          
+      IF(pl.post_id = p.id, true, false) AS isLiked,          
       GROUP_CONCAT(DISTINCT c.name ORDER BY c.name ASC SEPARATOR ',') AS categories
       FROM posts AS p
       INNER JOIN users AS u ON p.authorid = u.id
       INNER JOIN post_categories AS pc ON pc.postid = p.id 
       INNER JOIN categories AS c ON c.id = pc.categoryid
-      INNER JOIN post_likes AS pl ON pl.postid = p.id
-      GROUP BY p.id 
+      INNER JOIN post_likes AS pl ON pl.post_id = p.id
+      GROUP BY p.id;
       ORDER BY p.createddate DESC;`;
 
       const posts = await queryDb(query);
@@ -307,15 +307,14 @@ exports.get_post = [
       p.price, 
       p.createddate, 
       p.image,
-      IF(pl.postid = p.id, true, false) AS isLiked,          
+      IF(pl.post_id = p.id, true, false) AS isLiked,          
       GROUP_CONCAT(DISTINCT c.name ORDER BY c.name ASC SEPARATOR ',') AS categories
-  FROM posts AS p
-  INNER JOIN users AS u ON p.authorid = u.id
-  INNER JOIN post_categories AS pc ON pc.postid = p.id 
-  INNER JOIN categories AS c ON c.id = pc.categoryid
-  INNER JOIN post_likes AS pl ON pl.postid = p.id
-
-  GROUP BY p.id 
+    FROM posts AS p
+    INNER JOIN users AS u ON p.authorid = u.id
+    INNER JOIN post_categories AS pc ON pc.postid = p.id 
+    INNER JOIN categories AS c ON c.id = pc.categoryid
+    INNER JOIN post_likes AS pl ON pl.post_id = p.id
+    GROUP BY p.id;
   WHERE p.id = ?
   `;
 
