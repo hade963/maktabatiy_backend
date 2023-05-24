35702bd16efd7ade891683393ea6f42227a563cf
hade963 <86708561+hade963@users.noreply.github.com>
2023-05-24 17:35:18 +0300

    remove logout router

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const user_controler = require("../controlers/user");
 
 router.post("/signup", user_controler.user_signup);
 router.post("/login", user_controler.user_login);
-router.post('/logout', user_controler.user_logout);
 router.post('/cart', user_controler.add_to_cart);
 router.post('/photo', user_controler.add_profile_photo);
 router.delete('/cart', user_controler.remove_from_cart);
